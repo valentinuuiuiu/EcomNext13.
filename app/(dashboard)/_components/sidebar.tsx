e185d3f7c28081ddfbdc873a10f15f0f9dffeed7
valentinuuiuiu <ionutbaltag3@gmail.com>
2023-10-16 21:51:18 +0300

    On branch main

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,12 @@
+import { Logo } from "./logo"
+
+export const Sidebar = () =>{
+    return (
+        <div className="h-full border-r flex flex-col
+        overflow-y-auto bg-white shadow-sm">
+            <div className="p-6 ">
+                <Logo />
+            </div>
+        </div>
+    )
+}
